6f8be8576f6959a1fdd8ea62bcbcd9627196ec23
Manjinder <manjindersinghjohal@aol.com>
2021-01-12 00:36:40 +0530

    added javascript logic

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hi hello');
+let english;
+let punjabi;
+let computing;
+
+const getResult = () => {
+    english = parseInt(document.getElementById('english').value);
+    punjabi = parseInt(document.getElementById('punjabi').value);
+    computing = parseInt(document.getElementById('computing').value);
+
+    const average = (english + punjabi + computing) / 3;
+
+    if (average >= 85) {
+        document.getElementById('resultOutput').innerHTML = 'Your Grade is A! Keep it up';
+    } else if (average >= 70) {
+        document.getElementById('resultOutput').innerHTML = 'Your Grade is B! You can still improve';
+    } else {
+        document.getElementById('resultOutput').innerHTML = 'Your Grade is C! You need to work hard';
+    } 
+}
